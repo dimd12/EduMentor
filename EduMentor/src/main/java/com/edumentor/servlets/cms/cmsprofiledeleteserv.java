ec1177d561ebcb5691f6e62064b013838808436a
Adrian Manoil <adrian.manoil@iucosoft.com>
2025-02-12 00:21:54 +0200

    DELETE FUNCTIONALITYgit add EduMentor/src/main/java/com/edumentor/servlets/cms/cmsprofiledeleteserv.java !

---
+++ EduMentor/src/main/java/com/edumentor/servlets/cms/cmsprofiledeleteserv.java
@@ -0,0 +1,108 @@
+package com.edumentor.servlets.cms;
+
+import com.edumentor.models.User;
+import com.edumentor.services.UserServiceIntf;
+import com.edumentor.services.impl.UserServiceImpl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author adrian
+ */
+
+@WebServlet(name = "cmsprofiledeleteserv", urlPatterns = {"/cms/cmsprofiledeleteserv"})
+public class cmsprofiledeleteserv extends HttpServlet {
+
+    UserServiceIntf userService = UserServiceImpl.getInstance();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        try (PrintWriter out = response.getWriter()) {
+            HttpSession session = request.getSession(false);
+            if (session == null) {
+                throw new ServletException("Session not found. User may not be logged in.");
+            }
+
+            String currentuser = (String) session.getAttribute("CURRENTUSER");
+            if (currentuser == null) {
+                throw new ServletException("No user is logged in.");
+            }
+
+            UserServiceIntf userService = UserServiceImpl.getInstance();
+            User user = userService.findByUsername(currentuser);
+            if (user == null) {
+                throw new ServletException("User not found in the system.");
+            }
+
+            userService.delete(user.getUserId());
+
+            session.invalidate();
+
+            response.sendRedirect("../login.html");
+        } catch (Exception ex) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred during account deletion: " + ex.getMessage());
+        }
+
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
