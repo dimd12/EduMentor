b77904a82c2e19796f396075d4ac4a9ff1b49fda
Adrian Manoil <adrian.manoil@iucosoft.com>
2025-02-12 23:46:51 +0200

    Added admin question (filter wont work)

---
+++ EduMentor/src/main/java/com/edumentor/servlets/cms/adminquestionsserv.java
@@ -0,0 +1,109 @@
+package com.edumentor.servlets.cms;
+
+import com.edumentor.models.Question;
+import com.edumentor.models.User;
+import com.edumentor.services.QuestionServiceIntf;
+import com.edumentor.services.impl.QuestionServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "adminquestionsserv", urlPatterns = {"/admin/adminquestionsserv"})
+public class adminquestionsserv extends HttpServlet {
+
+    QuestionServiceIntf questionService = QuestionServiceImpl.getInstance();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+
+            HttpSession session = request.getSession();
+            if (session == null) {
+                throw new Exception("Session not found");
+            }
+
+            User currentUser = (User) session.getAttribute("CURRENTUSER");
+            if (currentUser == null) {
+                throw new Exception("The user is null");
+            }
+
+            if (currentUser.getRoleId() == null || currentUser.getRoleId().getRoleName() == null) {
+                throw new Exception("User role is not defined.");
+            }
+
+            String roleName = currentUser.getRoleId().getRoleName();
+
+            if(roleName.equalsIgnoreCase("admin")){
+                    List<Question> questionList = questionService.findAll();
+                request.setAttribute("questionList", questionList);
+
+                String path = "/WEB-INF/pages/cms/adminquestions.jsp";
+                request.getRequestDispatcher(path).forward(request, response);
+            } else if(roleName.equalsIgnoreCase("user")){
+                String errorPath = "/WEB-INF/pages/error.jsp";
+                request.getRequestDispatcher(errorPath).forward(request, response);
+            } else{
+                throw new Exception("Invalid role");
+            }
+        } catch (Exception e) {
+            response.sendRedirect("../login.html");
+        }
+
+    }
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
---
+++ EduMentor/src/main/webapp/WEB-INF/pages/cms/adminquestions.jsp
@@ -0,0 +1,70 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: adrian
+  Date: 2/12/25
+  Time: 10:39 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <%@include file="../common/head.jspf"%>
+    <%@include file="../common/styles.jspf"%>
+    <title>Post administration</title>
+
+    <style>
+        .navbar {
+            background-color: #e6e6fa; /* Lavender background as per shared styles */
+            box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); /* Retain the original shadow */
+        }
+
+        .navbar-brand, .navbar-nav .nav-link {
+            color: #4b0082 !important; /* Retain purple text for links */
+        }
+
+        .navbar-brand:hover, .navbar-nav .nav-link:hover {
+            color: #9370db !important; /* Lighter purple on hover */
+        }
+    </style>
+</head>
+<body>
+<%@include file="../common/navbarsidebarlogin.jspf"%>
+<div class="container mt-4">
+    <a class="bg-primary btn" href="show-add-question.html">Add article</a> |
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">Nr</th>
+            <th scope="col">Title</th>
+            <th scope="col">Date</th>
+            <th scope="col">Author</th>
+            <th scope="col">Image</th>
+            <th scope="col">Category</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach items="${questionList}" var="question">
+            <tr>
+                <th scope="row">${question.id}</th>
+                <td>${question.title}</td>
+                <td>${question.dateCreated}</td>
+                <td>${question.userId.username} ${question.userId.firstName}</td>
+                <td>
+                    <img src="${question.imageUrl}" alt="Image" width="100" height="100" />
+                </td>
+                <td>${question.categoryId.categoryName}</td>
+                <td>
+                    <a href="edit-question.html?id=${question.id}">Edit</a> |
+                    <a href="delete-question.html?id=${question.id}">Delete</a>
+                </td>
+            </tr>
+        </c:forEach>
+        </tbody>
+    </table>
+</div>
+<%@include file="../common/footer.jspf"%>
+<%@include file="../common/footerscripts.jspf"%>
+</body>
+</html>
